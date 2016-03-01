c9fb3413946223ebcae5e67f622c392035a0cdf4
mflehmig <martin.flehmig@tu-dresden.de>
2016-03-01 22:59:29 +0100

    - Coding Style

--- include/Model/FMUSimulate.hpp
+++ include/Model/FMUSimulate.hpp
@@ -1,28 +1,28 @@
 /*
-* Copyright (C) 2016, Volker Waurich
-*
-* This file is part of OMVis.
-*
-* OMVis is free software: you can redistribute it and/or modify
-* it under the terms of the GNU General Public License as published by
-* the Free Software Foundation, either version 3 of the License, or
-* (at your option) any later version.
-*
-* OMVis is distributed in the hope that it will be useful,
-* but WITHOUT ANY WARRANTY; without even the implied warranty of
-* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-* GNU General Public License for more details.
-*
-* You should have received a copy of the GNU General Public License
-* along with OMVis.  If not, see <http://www.gnu.org/licenses/>.
-*/
+ * Copyright (C) 2016, Volker Waurich
+ *
+ * This file is part of OMVis.
+ *
+ * OMVis is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * OMVis is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with OMVis.  If not, see <http://www.gnu.org/licenses/>.
+ */
 
 #ifndef INCLUDE_FMUSIMULATE_HPP_
 #define INCLUDE_FMUSIMULATE_HPP_
 
+#include <string>
 #include "WrapperFMILib.hpp"
 #include "Input.hpp"
-#include <iostream>
 //#include "OMVisualFMU.hpp"
 //#include "SimSettings.hpp"
 
@@ -38,38 +38,35 @@ namespace Model
 namespace Model
 {
 
-	typedef struct
-	{
-		fmi1_import_t* _fmu;
-		fmi_import_context_t* _context;  //MF Muss es fmi1_import_context_t* sein?
-		jm_callbacks* _callbacks;
-		fmi1_callback_functions_t _callBackFunctions;
-	} fmul_t;
+    typedef struct
+    {
+        fmi1_import_t* _fmu;
+        fmi_import_context_t* _context;  //MF Muss es fmi1_import_context_t* sein?
+        jm_callbacks* _callbacks;
+        fmi1_callback_functions_t _callBackFunctions;
+    } fmul_t;
 
-	typedef struct
-	{
-		fmi1_real_t* _states;
-		fmi1_real_t* _statesDer;
-		fmi1_real_t* _eventIndicators;
-		fmi1_real_t* _eventIndicatorsPrev;
-		size_t _nStates;
-		size_t _nEventIndicators;
-		fmi1_status_t _fmiStatus;
-		fmi1_event_info_t _eventInfo;
-		fmi1_real_t _tcur;
-		fmi1_real_t _hcur;
-	} fmuData;
+    typedef struct
+    {
+        fmi1_real_t* _states;
+        fmi1_real_t* _statesDer;
+        fmi1_real_t* _eventIndicators;
+        fmi1_real_t* _eventIndicatorsPrev;
+        size_t _nStates;
+        size_t _nEventIndicators;
+        fmi1_status_t _fmiStatus;
+        fmi1_event_info_t _eventInfo;
+        fmi1_real_t _tcur;
+        fmi1_real_t _hcur;
+    } fmuData;
 
-	//functions
+    //functions
 
-	fmuData initializeFMU(fmi1_import_t* fmu, Model::SimSettings* settings);
+    fmuData initializeFMU(fmi1_import_t* fmu, Model::SimSettings* settings);
 
-	fmul_t load(const char* FMUPath, const char* modelName);
+    fmul_t load(const char* FMUPath, const char* modelName);
 
-	fmi1_base_type_enu_t getFMI1baseTypeFor4CharString(std::string typeString);
-
-
-
+    fmi1_base_type_enu_t getFMI1baseTypeFor4CharString(std::string typeString);
 
 }  // End namespace Model
 
