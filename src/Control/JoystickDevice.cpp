d13865be4d1eafb1cd5b86ee954edb18ddf9be82
mflehmig <martin.flehmig@tu-dresden.de>
2016-04-19 09:13:32 +0200

    Improved code style.

--- src/Control/JoystickDevice.cpp
+++ src/Control/JoystickDevice.cpp
@@ -1,21 +1,21 @@
 /*
-* Copyright (C) 2016, Volker Waurich
-*
-* This file is part of OMVis.
-*
-* OMVis is free software: you can redistribute it and/or modify
-* it under the terms of the GNU General Public License as published by
-* the Free Software Foundation, either version 3 of the License, or
-* (at your option) any later version.
-*
-* OMVis is distributed in the hope that it will be useful,
-* but WITHOUT ANY WARRANTY; without even the implied warranty of
-* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-* GNU General Public License for more details.
-*
-* You should have received a copy of the GNU General Public License
-* along with OMVis.  If not, see <http://www.gnu.org/licenses/>.
-*/
+ * Copyright (C) 2016, Volker Waurich
+ *
+ * This file is part of OMVis.
+ *
+ * OMVis is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * OMVis is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with OMVis.  If not, see <http://www.gnu.org/licenses/>.
+ */
 
 #include "Control/JoystickDevice.hpp"
 #include "Model/InputData.hpp"
@@ -27,11 +27,11 @@
 
 namespace Control
 {
-	JoystickDevice::JoystickDevice(int joyID)
-		: _joystick(nullptr),
-		_xDir(0),
-		_yDir(0),
-		_joystickId(joyID)
+    JoystickDevice::JoystickDevice(int joyID)
+            : _joystick(nullptr),
+              _xDir(0),
+              _yDir(0),
+              _joystickId(joyID)
     {
         //Initialize SDL
         //if (SDL_Init(SDL_INIT_JOYSTICK) < 0)
@@ -39,26 +39,26 @@ namespace Control
 
         //Check for joysticks
         if (SDL_NumJoysticks() < 1)
-            LOGGER_WRITE(std::string("No joysticks connected!"),  Util::LC_LOADER, Util::LL_WARNING);
+            LOGGER_WRITE(std::string("No joysticks connected!"), Util::LC_LOADER, Util::LL_WARNING);
         else
         {
-            LOGGER_WRITE(std::string("Found ") + std::to_string(SDL_NumJoysticks()) + std::string(" joystick(s)"),  Util::LC_LOADER, Util::LL_INFO);
+            LOGGER_WRITE(std::string("Found ") + std::to_string(SDL_NumJoysticks()) + std::string(" joystick(s)"), Util::LC_LOADER, Util::LL_INFO);
             //Load joystick
-			_joystick = SDL_JoystickOpen(joyID);
-			if (_joystick == nullptr)
-				LOGGER_WRITE(std::string("Unable to open joystick! SDL Error: ") + SDL_GetError(), Util::LC_LOADER, Util::LL_INFO);
+            _joystick = SDL_JoystickOpen(joyID);
+            if (_joystick == nullptr)
+                LOGGER_WRITE(std::string("Unable to open joystick! SDL Error: ") + SDL_GetError(), Util::LC_LOADER, Util::LL_INFO);
         }
     }
 
-	int JoystickDevice::getXDir() 
-	{
-		return _xDir;
-	}
+    int JoystickDevice::getXDir()
+    {
+        return _xDir;
+    }
 
-	int JoystickDevice::getYDir()
-	{
-		return _yDir;
-	}
+    int JoystickDevice::getYDir()
+    {
+        return _yDir;
+    }
 
     void JoystickDevice::detectContinuousInputEvents(Model::InputData inputInfo)
     {
@@ -68,26 +68,26 @@ namespace Control
         if (_inputEvent.type == SDL_JOYAXISMOTION)
         {
             //Motion on first joystick
-			if (_inputEvent.jaxis.which == _joystickId)
-			{
-				//X axis motion
-				if (_inputEvent.jaxis.axis == 0)
-				{
-					_xDir = _inputEvent.jaxis.value;
-					Model::setRealInputValueForInputKey(inputKey(_joystickId), _xDir, inputInfo);
-				}
-				//Y axis motion
-				else if (_inputEvent.jaxis.axis == 1)
-				{
-					_yDir = _inputEvent.jaxis.value;
-					Model::setRealInputValueForInputKey(inputKey(_joystickId+1), _yDir, inputInfo);
-				}
-				else
-				{
-					_xDir = 0;
-					_yDir = 0;
-				}
-			}
+            if (_inputEvent.jaxis.which == _joystickId)
+            {
+                //X axis motion
+                if (_inputEvent.jaxis.axis == 0)
+                {
+                    _xDir = _inputEvent.jaxis.value;
+                    Model::setRealInputValueForInputKey(inputKey(_joystickId), _xDir, inputInfo);
+                }
+                //Y axis motion
+                else if (_inputEvent.jaxis.axis == 1)
+                {
+                    _yDir = _inputEvent.jaxis.value;
+                    Model::setRealInputValueForInputKey(inputKey(_joystickId + 1), _yDir, inputInfo);
+                }
+                else
+                {
+                    _xDir = 0;
+                    _yDir = 0;
+                }
+            }
         }
     }
 
