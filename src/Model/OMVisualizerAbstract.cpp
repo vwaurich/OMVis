4acf040562172262a4c13a31352e4cd69ae8c73b
mflehmig <martin.flehmig@tu-dresden.de>
2016-04-22 09:33:20 +0200

    Clean up of old code.

--- src/Model/OMVisualizerAbstract.cpp
+++ src/Model/OMVisualizerAbstract.cpp
@@ -24,8 +24,7 @@ namespace Model
     }
 
     OMVisualizerAbstract::OMVisualizerAbstract(const std::string modelName, const std::string path)
-            : //X2 _baseData(new OMVisualBase(modelName, path)),
-              _viewerStuff(new View::OMVisScene()),
+            : _viewerStuff(new View::OMVisScene()),
               _nodeUpdater(new Model::UpdateVisitor()),
               _omvManager(new Control::OMVisManager(0.0, 0.0, 0.0, 0.1, 0.0, 100.0))
     {
