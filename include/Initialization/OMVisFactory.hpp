49d1ca9200e7ecce5adcbd503040fe96077fd20d
mflehmig <martin.flehmig@tu-dresden.de>
2016-04-22 15:44:38 +0200

    Refactored Factory to OMVisFactory.
    This might be the better name for this class.

---
+++ include/Initialization/OMVisFactory.hpp
@@ -0,0 +1,90 @@
+/*
+ * Copyright (C) 2016, Volker Waurich
+ *
+ * This file is part of OMVis.
+ *
+ * OMVis is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * OMVis is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with OMVis.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/** @addtogroup Initialization
+ *  @{
+ *  \copyright TU Dresden. All rights reserved.
+ *  \authors Volker Waurich, Martin Flehmig
+ *  \date Feb 2016
+ */
+
+#ifndef INCLUDE_FACTORY_HPP_
+#define INCLUDE_FACTORY_HPP_
+
+#include <string>
+
+// Forward declarations
+namespace OMVIS
+{
+    namespace Model
+    {
+        class OMVisualizerAbstract;
+    }
+    namespace Util
+    {
+        class CommandLineArgs;
+    }
+    namespace Control
+    {
+        class OMVisManager;
+    }
+}
+
+namespace OMVIS
+{
+    namespace Initialization
+    {
+        /*! \brief This factory class can create OMVisualization objects that are required for an FMU or result file based visualization.
+         *
+         */
+        class Factory
+        {
+         public:
+            Factory() = default;
+            ~Factory() = default;
+            Factory(const Factory& f) = delete;
+            Factory& operator=(const Factory& f) = delete;
+
+            /*! \brief Creates concrete Visualization object according to the command line arguments.
+             *
+             * @param cLArgs The parsed command line arguments.
+             * @return Visualization object of type OMVisualizationMAT or OMVisualizationFMU.
+             */
+            Model::OMVisualizerAbstract* createVisualizationFromCLargs(const Util::CommandLineArgs& cLArgs);
+
+            /*! \brief Creates new visualization object according to the given command line parameters.
+             *
+             * According to the command line parameters a OMVisualizerFMU or OMVisualizerMAT object is created and a pointer to this
+             * object is returned. If the command line is empty, than nullptr is returned.
+             *
+             * @param modelName
+             * @param pathName
+             * @param useFMU
+             * @return
+             */
+            Model::OMVisualizerAbstract* createVisualizationObject(const std::string modelName = "", const std::string pathName = "", const bool useFMU = false);
+        };
+
+    }  // End namespace Initialization
+}  // End namespace OMVIS
+
+#endif /* INCLUDE_FACTORY_HPP_ */
+/**
+ * @}
+ */
---
+++ src/Initialization/OMVisFactory.cpp
@@ -0,0 +1,72 @@
+/*
+ * Copyright (C) 2016, Volker Waurich
+ *
+ * This file is part of OMVis.
+ *
+ * OMVis is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * OMVis is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with OMVis.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include <Initialization/OMVisFactory.hpp>
+#include "Model/OMVisualizerFMU.hpp"
+#include "Model/OMVisualizerMAT.hpp"
+#include "Model/OMVisualizerAbstract.hpp"
+#include "Util/Logger.hpp"
+#include "Util/Util.hpp"
+#include "Util/CommandLineArgs.hpp"
+#include "Control/OMVisManager.hpp"
+
+namespace OMVIS
+{
+    namespace Initialization
+    {
+
+        Model::OMVisualizerAbstract* Factory::createVisualizationFromCLargs(const Util::CommandLineArgs& cLArgs)
+        {
+            return createVisualizationObject(cLArgs._modelName, cLArgs._modelPath, cLArgs._useFMU);
+        }
+
+        Model::OMVisualizerAbstract* Factory::createVisualizationObject(const std::string modelName, const std::string path, const bool useFMU)
+        {
+            Model::OMVisualizerAbstract* result(nullptr);
+
+            // Command line is empty. Model has to be loaded via GUI. Return nullptr.
+            if (modelName.empty() && path.empty())
+            {
+                LOGGER_WRITE("Initialize OMVisalizerAbstract because path and model name are empty.", Util::LC_LOADER, Util::LL_DEBUG);
+            }
+            else if (Util::exists(path))
+            {
+                //FMU based visualization
+                if (useFMU)
+                {
+                    result = (Model::OMVisualizerAbstract*) (new Model::OMVisualizerFMU(modelName, path));
+                    LOGGER_WRITE("Initialize OMVisalizerFMU.", Util::LC_LOADER, Util::LL_DEBUG);
+                }
+                //Mat-file based visualization
+                else
+                {
+                    result = (Model::OMVisualizerAbstract*) (new Model::OMVisualizerMAT(modelName, path));
+                    LOGGER_WRITE("Initialize OMVisalizerMAT.", Util::LC_LOADER, Util::LL_DEBUG);
+                }
+            }
+            else
+            {
+                LOGGER_WRITE("Initialize OMVisalizerAbstract because path " + path + std::string(" can not be accessed."), Util::LC_LOADER, Util::LL_DEBUG);
+            }
+            return result;
+        }
+
+    }  // End namespace Initialization
+}  // End namespace OMVIS
+
