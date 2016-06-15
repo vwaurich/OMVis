f19aa9398d7278192dba376ff06c29c3bab4e9fd
mflehmig <martin.flehmig@tu-dresden.de>
2016-06-15 11:00:14 +0200

    Add dialog for choosing model file.
    This dialog implements the File->Open dialog.

---
+++ include/View/Dialogs/OpenFileDialog.hpp
@@ -0,0 +1,85 @@
+/*
+ * Copyright (C) 2016, Volker Waurich
+ *
+ * This file is part of OMVis.
+ *
+ * OMVis is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * OMVis is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with OMVis.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef INCLUDE_VIEW_DIALOGS_OPENFILEDIALOG_HPP_
+#define INCLUDE_VIEW_DIALOGS_OPENFILEDIALOG_HPP_
+
+#include "Initialization/VisualizationConstructionPlan.hpp"
+
+#include <QString>
+#include <QFileDialog>
+
+#include <string>
+
+namespace OMVIS
+{
+    namespace View
+    {
+        /*! \brief This class provides a dialog to select a simulation model file from local file system.
+         *
+         */
+        class OpenFileDialog : public QFileDialog
+        {
+        Q_OBJECT
+
+         public:
+            /*-----------------------------------------
+             * CONSTRUCTORS
+             *---------------------------------------*/
+
+            /*! \brief Constructor which creates the Qt dialog window to select the simulation file.
+             *
+             * The user selects the simulation file from the local file system.
+             */
+            OpenFileDialog(QWidget* parent = Q_NULLPTR);
+
+            /*-----------------------------------------
+             * SLOTS
+             *---------------------------------------*/
+
+         private slots:
+            /*! \brief Accepts user input and stores it into member variables. */
+            void accept() Q_DECL_OVERRIDE;
+
+            /*-----------------------------------------
+             * GETTERS AND SETTERS
+             *---------------------------------------*/
+
+//            /*! \brief Returns the file name (incl. path) of the selected model.
+//             *
+//             * @return The model file name
+//             */
+//            std::string getModelFileName() const;
+
+         public:
+            /*! \brief Returns the construction plan for the visualization of the chosen model. */
+            Initialization::VisualizationConstructionPlan getConstructionPlan() const;
+
+         private:
+            /*-----------------------------------------
+             * MEMBERS
+             *---------------------------------------*/
+
+            Initialization::VisualizationConstructionPlan _cP;
+        };
+
+    }  // End namespace View
+}  // End namespace OMVIS
+
+#endif /* INCLUDE_VIEW_DIALOGS_OPENFILEDIALOG_HPP_ */
