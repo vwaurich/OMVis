edec52a3ddeee567586b38c42329a196e0ae1517
mflehmig <martin.flehmig@tu-dresden.de>
2016-06-24 15:35:41 +0200

    Refactor class name to make clear, it belongs to ShapeObject class

--- include/Model/ShapeObject.hpp
+++ include/Model/ShapeObject.hpp
@@ -1,30 +1,30 @@
 /*
-* Copyright (C) 2016, Volker Waurich
-*
-* This file is part of OMVis.
-*
-* OMVis is free software: you can redistribute it and/or modify
-* it under the terms of the GNU General Public License as published by
-* the Free Software Foundation, either version 3 of the License, or
-* (at your option) any later version.
-*
-* OMVis is distributed in the hope that it will be useful,
-* but WITHOUT ANY WARRANTY; without even the implied warranty of
-* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-* GNU General Public License for more details.
-*
-* You should have received a copy of the GNU General Public License
-* along with OMVis.  If not, see <http://www.gnu.org/licenses/>.
-*/
-
+ * Copyright (C) 2016, Volker Waurich
+ *
+ * This file is part of OMVis.
+ *
+ * OMVis is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * OMVis is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with OMVis.  If not, see <http://www.gnu.org/licenses/>.
+ */
 
 #ifndef INCLUDE_SHAPEOBJECT_HPP_
 #define INCLUDE_SHAPEOBJECT_HPP_
 
 #include "WrapperFMILib.hpp"
+#include "ShapeObjectAttribute.hpp"
+
 #include <read_matlab4.h>
 #include <rapidxml.hpp>
-#include "Util/ObjectAttribute.hpp"
 
 #include <osg/Vec3f>
 #include <osg/Matrix>
@@ -34,43 +34,44 @@
 
 namespace OMVIS
 {
-	namespace Model
-	{
-		/*! \brief Contains all information to display a shape.
-		*
-		*
-		*/
-		class ShapeObject
-		{
-		public:
-			ShapeObject();
-			~ShapeObject() = default;
+    namespace Model
+    {
+        /*! \brief Contains all information to display a shape.
+         *
+         *
+         */
+        class ShapeObject
+        {
+         public:
+            ShapeObject();
+            ~ShapeObject() = default;
 
-			ShapeObject& operator=(const ShapeObject& va) = default;
+            ShapeObject& operator=(const ShapeObject& va) = default;
 
-			/// Dumps the attributes to std::cout.
-			void dumpVisAttributes() const;
+            /// Dumps the attributes to std::cout.
+            void dumpVisAttributes() const;
 
-			/// Computes the attributes of the shape for a certain time.
-			void fetchVisAttributes(rapidxml::xml_node<>* node, ModelicaMatReader matReader, fmi1_import_t* fmu, double time, bool useFMU);
+            /// Computes the attributes of the shape for a certain time.
+            void fetchVisAttributes(rapidxml::xml_node<>* node, ModelicaMatReader matReader, fmi1_import_t* fmu, double time, bool useFMU);
 
-		public:
-			std::string _id;
-			std::string _type;
-			ObjectAttribute _length;
-			ObjectAttribute _width;
-			ObjectAttribute _height;
-			ObjectAttribute _r[3];
-			ObjectAttribute _rShape[3];
-			ObjectAttribute _lDir[3];
-			ObjectAttribute _wDir[3];
-			ObjectAttribute _color[3];
-			ObjectAttribute _T[9];
-			ObjectAttribute	_specCoeff;
+         public:
+            std::string _id;
+            std::string _type;
+            ShapeObjectAttribute _length;
+            ShapeObjectAttribute _width;
+            ShapeObjectAttribute _height;
+            ShapeObjectAttribute _r[3];
+            ShapeObjectAttribute _rShape[3];
+            ShapeObjectAttribute _lDir[3];
+            ShapeObjectAttribute _wDir[3];
+            ShapeObjectAttribute _color[3];
+            ShapeObjectAttribute _T[9];
+            ShapeObjectAttribute _specCoeff;
 
-			osg::Matrix _mat;
-		};
+            osg::Matrix _mat;
+        };
 
-	}; //namespace Model;
-}; //namespace OMVVIS;
-#endif
+    } // End namespace Model;
+} // End namespace OMVIS;
+
+#endif /* INCLUDE_SHAPEOBJECT_HPP_ */
--- src/Model/ShapeObject.cpp
+++ src/Model/ShapeObject.cpp
@@ -1,21 +1,21 @@
 /*
-* Copyright (C) 2016, Volker Waurich
-*
-* This file is part of OMVis.
-*
-* OMVis is free software: you can redistribute it and/or modify
-* it under the terms of the GNU General Public License as published by
-* the Free Software Foundation, either version 3 of the License, or
-* (at your option) any later version.
-*
-* OMVis is distributed in the hope that it will be useful,
-* but WITHOUT ANY WARRANTY; without even the implied warranty of
-* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-* GNU General Public License for more details.
-*
-* You should have received a copy of the GNU General Public License
-* along with OMVis.  If not, see <http://www.gnu.org/licenses/>.
-*/
+ * Copyright (C) 2016, Volker Waurich
+ *
+ * This file is part of OMVis.
+ *
+ * OMVis is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * OMVis is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with OMVis.  If not, see <http://www.gnu.org/licenses/>.
+ */
 
 #include "Model/ShapeObject.hpp"
 #include "Visualize.hpp"
@@ -24,65 +24,64 @@
 
 namespace OMVIS
 {
-	namespace Model
-	{
+    namespace Model
+    {
 
-		ShapeObject::ShapeObject()
-			:
-			_id("noID"),
-			_type("box"),
-			_length(ObjectAttribute(0.1)),
-			_width(ObjectAttribute(0.1)),
-			_height(ObjectAttribute(0.1))
-		{
-			_r[0] = ObjectAttribute(0.1);
-			_r[1] = ObjectAttribute(0.1);
-			_r[2] = ObjectAttribute(0.1);
-			_rShape[0] = ObjectAttribute(0.0);
-			_rShape[1] = ObjectAttribute(0.0);
-			_rShape[2] = ObjectAttribute(0.0);
-			_lDir[0] = ObjectAttribute(1.0);
-			_lDir[1] = ObjectAttribute(0.0);
-			_lDir[2] = ObjectAttribute(0.0);
-			_wDir[0] = ObjectAttribute(0.0);
-			_wDir[1] = ObjectAttribute(1.0);
-			_wDir[2] = ObjectAttribute(0.0);
-			_color[0] = ObjectAttribute(255.0);
-			_color[1] = ObjectAttribute(255.0);
-			_color[2] = ObjectAttribute(255.0);
-			_T[0] = ObjectAttribute(0.0);
-			_T[1] = ObjectAttribute(0.0);
-			_T[2] = ObjectAttribute(1.0);
-			_T[3] = ObjectAttribute(1.0);
-			_T[4] = ObjectAttribute(0.0);
-			_T[5] = ObjectAttribute(0.0);
-			_T[6] = ObjectAttribute(0.0);
-			_T[7] = ObjectAttribute(1.0);
-			_T[8] = ObjectAttribute(0.0);
-			_specCoeff = ObjectAttribute(0.7);
+        ShapeObject::ShapeObject()
+                : _id("noID"),
+                  _type("box"),
+                  _length(ShapeObjectAttribute(0.1)),
+                  _width(ShapeObjectAttribute(0.1)),
+                  _height(ShapeObjectAttribute(0.1))
+        {
+            _r[0] = ShapeObjectAttribute(0.1);
+            _r[1] = ShapeObjectAttribute(0.1);
+            _r[2] = ShapeObjectAttribute(0.1);
+            _rShape[0] = ShapeObjectAttribute(0.0);
+            _rShape[1] = ShapeObjectAttribute(0.0);
+            _rShape[2] = ShapeObjectAttribute(0.0);
+            _lDir[0] = ShapeObjectAttribute(1.0);
+            _lDir[1] = ShapeObjectAttribute(0.0);
+            _lDir[2] = ShapeObjectAttribute(0.0);
+            _wDir[0] = ShapeObjectAttribute(0.0);
+            _wDir[1] = ShapeObjectAttribute(1.0);
+            _wDir[2] = ShapeObjectAttribute(0.0);
+            _color[0] = ShapeObjectAttribute(255.0);
+            _color[1] = ShapeObjectAttribute(255.0);
+            _color[2] = ShapeObjectAttribute(255.0);
+            _T[0] = ShapeObjectAttribute(0.0);
+            _T[1] = ShapeObjectAttribute(0.0);
+            _T[2] = ShapeObjectAttribute(1.0);
+            _T[3] = ShapeObjectAttribute(1.0);
+            _T[4] = ShapeObjectAttribute(0.0);
+            _T[5] = ShapeObjectAttribute(0.0);
+            _T[6] = ShapeObjectAttribute(0.0);
+            _T[7] = ShapeObjectAttribute(1.0);
+            _T[8] = ShapeObjectAttribute(0.0);
+            _specCoeff = ShapeObjectAttribute(0.7);
 
-			_mat = osg::Matrix(0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0);
-		}
+            _mat = osg::Matrix(0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0);
+        }
 
-		void ShapeObject::dumpVisAttributes() const
-		{
-			std::cout << "id " << _id << std::endl;
-			std::cout << "type " << _type << std::endl;
-			std::cout << "length " << _length.getValueString() << std::endl;
-			std::cout << "width " << _width.getValueString() << std::endl;
-			std::cout << "height " << _height.getValueString() << std::endl;
-			std::cout << "lDir " << _lDir[0].getValueString() << ", " << _lDir[1].getValueString() << ", " << _lDir[2].getValueString() << ", " << std::endl;
-			std::cout << "wDir " << _wDir[0].getValueString() << ", " << _wDir[1].getValueString() << ", " << _wDir[2].getValueString() << ", " << std::endl;
-			std::cout << "r " << _r[0].getValueString() << ", " << _r[1].getValueString() << ", " << _r[2].getValueString() << ", " << std::endl;
-			std::cout << "r_shape " << _rShape[0].getValueString() << ", " << _rShape[1].getValueString() << ", " << _rShape[2].getValueString() << ", " << std::endl;
-			std::cout << "T0 " << _T[0].getValueString() << ", " << _T[1].getValueString() << ", " << _T[2].getValueString() << ", " << std::endl;
-			std::cout << "   " << _T[3].getValueString() << ", " << _T[4].getValueString() << ", " << _T[5].getValueString() << ", " << std::endl;
-			std::cout << "   " << _T[6].getValueString() << ", " << _T[7].getValueString() << ", " << _T[8].getValueString() << ", " << std::endl;
-			std::cout << "color " << _color[0].getValueString() << ", " << _color[1].getValueString() << ", " << _color[2].getValueString() << ", " << std::endl;
-			std::cout << "mat " << _mat(0, 0) << ", " << _mat(0, 1) << ", " << _mat(0, 2) << ", " << _mat(0, 3) << std::endl;
-			std::cout << "    " << _mat(1, 0) << ", " << _mat(1, 1) << ", " << _mat(1, 2) << ", " << _mat(1, 3) << std::endl;
-			std::cout << "    " << _mat(2, 0) << ", " << _mat(2, 1) << ", " << _mat(2, 2) << ", " << _mat(2, 3) << std::endl;
-			std::cout << "    " << _mat(3, 0) << ", " << _mat(3, 1) << ", " << _mat(3, 2) << ", " << _mat(3, 3) << std::endl;
-		}
-	}
+        void ShapeObject::dumpVisAttributes() const
+        {
+            std::cout << "id " << _id << std::endl;
+            std::cout << "type " << _type << std::endl;
+            std::cout << "length " << _length.getValueString() << std::endl;
+            std::cout << "width " << _width.getValueString() << std::endl;
+            std::cout << "height " << _height.getValueString() << std::endl;
+            std::cout << "lDir " << _lDir[0].getValueString() << ", " << _lDir[1].getValueString() << ", " << _lDir[2].getValueString() << ", " << std::endl;
+            std::cout << "wDir " << _wDir[0].getValueString() << ", " << _wDir[1].getValueString() << ", " << _wDir[2].getValueString() << ", " << std::endl;
+            std::cout << "r " << _r[0].getValueString() << ", " << _r[1].getValueString() << ", " << _r[2].getValueString() << ", " << std::endl;
+            std::cout << "r_shape " << _rShape[0].getValueString() << ", " << _rShape[1].getValueString() << ", " << _rShape[2].getValueString() << ", " << std::endl;
+            std::cout << "T0 " << _T[0].getValueString() << ", " << _T[1].getValueString() << ", " << _T[2].getValueString() << ", " << std::endl;
+            std::cout << "   " << _T[3].getValueString() << ", " << _T[4].getValueString() << ", " << _T[5].getValueString() << ", " << std::endl;
+            std::cout << "   " << _T[6].getValueString() << ", " << _T[7].getValueString() << ", " << _T[8].getValueString() << ", " << std::endl;
+            std::cout << "color " << _color[0].getValueString() << ", " << _color[1].getValueString() << ", " << _color[2].getValueString() << ", " << std::endl;
+            std::cout << "mat " << _mat(0, 0) << ", " << _mat(0, 1) << ", " << _mat(0, 2) << ", " << _mat(0, 3) << std::endl;
+            std::cout << "    " << _mat(1, 0) << ", " << _mat(1, 1) << ", " << _mat(1, 2) << ", " << _mat(1, 3) << std::endl;
+            std::cout << "    " << _mat(2, 0) << ", " << _mat(2, 1) << ", " << _mat(2, 2) << ", " << _mat(2, 3) << std::endl;
+            std::cout << "    " << _mat(3, 0) << ", " << _mat(3, 1) << ", " << _mat(3, 2) << ", " << _mat(3, 3) << std::endl;
+        }
+    }
 }
