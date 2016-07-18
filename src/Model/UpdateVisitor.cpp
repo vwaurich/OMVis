f309bcefb41cc0caec648060fbdb3091634142b9
mflehmig <martin.flehmig@tu-dresden.de>
2016-07-18 13:53:42 +0200

    Harmonize code style

--- src/Model/UpdateVisitor.cpp
+++ src/Model/UpdateVisitor.cpp
@@ -27,74 +27,74 @@
 
 namespace OMVIS
 {
-namespace Model
-{
-
-    UpdateVisitor::UpdateVisitor()
-            : _shape()
+    namespace Model
     {
-        setTraversalMode(NodeVisitor::TRAVERSE_ALL_CHILDREN);
-    }
 
-    /**
-     MatrixTransform
-     */
-    void UpdateVisitor::apply(osg::MatrixTransform& node)
-    {
-        //std::cout<<"MT "<<node.className()<<"  "<<node.getName()<<std::endl;
-        node.setMatrix(_shape._mat);
-        traverse(node);
-    }
-
-    /**
-     Geode
-     */
-    void UpdateVisitor::apply(osg::Geode& node)
-    {
-        //std::cout<<"GEODE "<< _shape._id<<" "<<std::endl;
-        osg::ref_ptr<osg::StateSet> ss = node.getOrCreateStateSet();
-
-        //its a stl-file
-        if (Util::isCADType(_shape._type))
+        UpdateVisitor::UpdateVisitor()
+                : _shape()
         {
-            std::string filename = Util::extractCADFilename(_shape._type);
-            osg::ref_ptr<osg::Node> node = osgDB::readNodeFile(filename);
+            setTraversalMode(NodeVisitor::TRAVERSE_ALL_CHILDREN);
+        }
 
-        }
-        //its a drawable
-        else
+        /**
+         MatrixTransform
+         */
+        void UpdateVisitor::apply(osg::MatrixTransform& node)
         {
-            osg::ref_ptr<osg::Drawable> draw = node.getDrawable(0);
-            draw->dirtyDisplayList();
-            //osg::ref_ptr<osg::ShapeDrawable> shapeDraw = dynamic_cast<osg::ShapeDrawable*>(draw.get());
-            //shapeDraw->setColor(osg::Vec4(visAttr.color,1.0));
+            //std::cout<<"MT "<<node.className()<<"  "<<node.getName()<<std::endl;
+            node.setMatrix(_shape._mat);
+            traverse(node);
+        }
 
-        if (_shape._type == "pipecylinder")
-            draw->setShape(new osg::Cylinder(osg::Vec3f(0.0, 0.0, 0.0), _shape._width.exp / 2.0, _shape._length.exp));
-        else if (_shape._type == "cylinder")
-            draw->setShape(new osg::Cylinder(osg::Vec3f(0.0, 0.0, 0.0), _shape._width.exp / 2.0, _shape._length.exp));
-        else if (_shape._type == "box")
-            draw->setShape(new osg::Box(osg::Vec3f(0.0, 0.0, 0.0), _shape._width.exp, _shape._height.exp, _shape._length.exp));
-        else if (_shape._type == "cone")
-            draw->setShape(new osg::Cone(osg::Vec3f(0.0, 0.0, 0.0), _shape._width.exp / 2.0, _shape._length.exp));
-        else if (_shape._type == "sphere")
-            draw->setShape(new osg::Sphere(osg::Vec3f(0.0, 0.0, 0.0), _shape._length.exp / 2.0));
-        else
+        /**
+         Geode
+         */
+        void UpdateVisitor::apply(osg::Geode& node)
         {
-            LOGGER_WRITE(std::string("Unknown type, we make a capsule."), Util::LC_GUI, Util::LL_WARNING);
-            //string id = string(visAttr.type.begin(), visAttr.type.begin()+11);
-            draw->setShape(new osg::Capsule(osg::Vec3f(0.0, 0.0, 0.0), 0.1, 0.5));
+            //std::cout<<"GEODE "<< _shape._id<<" "<<std::endl;
+            osg::ref_ptr<osg::StateSet> ss = node.getOrCreateStateSet();
+
+            //its a stl-file
+            if (Util::isCADType(_shape._type))
+            {
+                std::string filename = Util::extractCADFilename(_shape._type);
+                osg::ref_ptr<osg::Node> node = osgDB::readNodeFile(filename);
+
+            }
+            //its a drawable
+            else
+            {
+                osg::ref_ptr<osg::Drawable> draw = node.getDrawable(0);
+                draw->dirtyDisplayList();
+                //osg::ref_ptr<osg::ShapeDrawable> shapeDraw = dynamic_cast<osg::ShapeDrawable*>(draw.get());
+                //shapeDraw->setColor(osg::Vec4(visAttr.color,1.0));
+
+                if (_shape._type == "pipecylinder")
+                    draw->setShape(new osg::Cylinder(osg::Vec3f(0.0, 0.0, 0.0), _shape._width.exp / 2.0, _shape._length.exp));
+                else if (_shape._type == "cylinder")
+                    draw->setShape(new osg::Cylinder(osg::Vec3f(0.0, 0.0, 0.0), _shape._width.exp / 2.0, _shape._length.exp));
+                else if (_shape._type == "box")
+                    draw->setShape(new osg::Box(osg::Vec3f(0.0, 0.0, 0.0), _shape._width.exp, _shape._height.exp, _shape._length.exp));
+                else if (_shape._type == "cone")
+                    draw->setShape(new osg::Cone(osg::Vec3f(0.0, 0.0, 0.0), _shape._width.exp / 2.0, _shape._length.exp));
+                else if (_shape._type == "sphere")
+                    draw->setShape(new osg::Sphere(osg::Vec3f(0.0, 0.0, 0.0), _shape._length.exp / 2.0));
+                else
+                {
+                    LOGGER_WRITE(std::string("Unknown type, we make a capsule."), Util::LC_GUI, Util::LL_WARNING);
+                    //string id = string(visAttr.type.begin(), visAttr.type.begin()+11);
+                    draw->setShape(new osg::Capsule(osg::Vec3f(0.0, 0.0, 0.0), 0.1, 0.5));
+                }
+                //std::cout<<"SHAPE "<<draw->getShape()->className()<<std::endl;
+                node.addDrawable(draw.get());
+            }
+            //osg::Material *material = dynamic_cast<osg::Material*>(ss->getAttribute(osg::StateAttribute::MATERIAL));
+            osg::ref_ptr<osg::Material> material = new osg::Material;
+            material->setDiffuse(osg::Material::FRONT, osg::Vec4f(_shape._color[0].exp / 255, _shape._color[1].exp / 255, _shape._color[2].exp / 255, 1.0));
+            ss->setAttribute(material);
+            node.setStateSet(ss);
+            traverse(node);
         }
-            //std::cout<<"SHAPE "<<draw->getShape()->className()<<std::endl;
-            node.addDrawable(draw.get());
-        }
-        //osg::Material *material = dynamic_cast<osg::Material*>(ss->getAttribute(osg::StateAttribute::MATERIAL));
-		osg::ref_ptr<osg::Material> material = new osg::Material;
-		material->setDiffuse(osg::Material::FRONT, osg::Vec4f(_shape._color[0].exp/255, _shape._color[1].exp / 255, _shape._color[2].exp / 255, 1.0));
-        ss->setAttribute(material);
-        node.setStateSet(ss);
-        traverse(node);
-    }
 
     }  // End namespace Model
 }  // End namespace OMVIS
