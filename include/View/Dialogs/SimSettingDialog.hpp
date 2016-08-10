8ac9ed867140c71d36181a9055d88443efe4ba59
mflehmig <martin.flehmig@tu-dresden.de>
2016-08-10 13:13:34 +0200

    Make use of visualization type and add dialog for simulation settings

--- include/View/Dialogs/SimSettingDialog.hpp
+++ include/View/Dialogs/SimSettingDialog.hpp
@@ -20,40 +20,80 @@
 #ifndef INCLUDE_VIEW_DIALOGS_SIMSETTINGDIALOG_HPP_
 #define INCLUDE_VIEW_DIALOGS_SIMSETTINGDIALOG_HPP_
 
+#include "Model/VisualizationTypes.hpp"
+
 #include <QDialog>
 #include <QLineEdit>
 #include <QComboBox>
 
 #include <memory>
+#include <string>
 
 namespace OMVIS
 {
     namespace View
     {
 
-        /*! \brief Class that encapsulates the dialog to specify the simulation settings.
+        // \todo Move to appropriate place
+        enum Solver
+        {
+            forwardEuler = 0
+        };
+
+
+        struct SimSettingsFMU
+        {
+            Solver solver;
+            float simStepSize;
+            float visStepSize;
+        };
+
+        struct SimSettingsMAT
+        {
+            float speedup;
+        };
+
+
+        /*! \brief This is a dialog to specify the simulation settings for FMU visualization.
          *
+         * The user can specify the simulation step size, the render frequency and the solver method.
          */
-        class SimSettingDialog : public QDialog
+        class SimSettingDialogFMU : public QDialog
         {
         Q_OBJECT
-
          public:
-            /*! \brief Constructor which creates the Qt dialog window to set simulation settings.
-             *
-             */
-			 SimSettingDialog(QWidget* parent = Q_NULLPTR);
+            SimSettingDialogFMU(QWidget* parent = Q_NULLPTR);
+            SimSettingsFMU getSimSettings() const;
 
-			 ~SimSettingDialog() = default;
-
-			 SimSettingDialog(const SimSettingDialog&) = delete;
-			 SimSettingDialog& operator=(const SimSettingDialog&) = delete;
+         private slots:
+            void accept() Q_DECL_OVERRIDE;
 
          private:
-            /*! Qt combo box which provides the simsettings to set. */
-            std::unique_ptr<QComboBox> _solverMethodBox;
-            std::unique_ptr<QLineEdit> _simStepSize;
-            std::unique_ptr<QLineEdit> _visStepSize;
+            QComboBox* _solverBox;
+            QLineEdit* _simStepSizeLineEdit;
+            QLineEdit* _visStepSizeLineEdit;
+            SimSettingsFMU _simSet;
+        };
+
+        /*! \brief This is a dialog to specify the simulation settings for MAT file visualization.
+         *
+         * At the moment, the user can set the speed of the visualization by choosing a speed up value. The
+         * visualization of the simulation result is slowed down if speed up < 1 and accelerated if the speed up > 1,
+         * respectively.
+         */
+        class SimSettingDialogMAT : public QDialog
+        {
+        Q_OBJECT
+         public:
+            SimSettingDialogMAT(QWidget* parent = Q_NULLPTR);
+            SimSettingsMAT getSimSettings() const;
+
+         private slots:
+            void accept() Q_DECL_OVERRIDE;
+
+         private:
+            QLineEdit* _speedupLineEdit;
+            SimSettingsMAT _simSet;
         };
 
     }  // End namespace View
--- src/View/Dialogs/SimSettingDialog.cpp
+++ src/View/Dialogs/SimSettingDialog.cpp
@@ -17,52 +17,130 @@
  * along with OMVis.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-
 #include "View/Dialogs/SimSettingDialog.hpp"
+#include "Util/Util.hpp"
 
 #include <QGroupBox>
 #include <QLabel>
 #include <QHBoxLayout>
 #include <QDebug>
 #include <QDialogButtonBox>
+#include <QMessageBox>
+#include <QString>
+
+#include <iostream>
 
 namespace OMVIS
 {
     namespace View
     {
-
-		SimSettingDialog::SimSettingDialog(QWidget* parent)
-			: QDialog(parent),
-			  _solverMethodBox(new QComboBox()),
- 			  _simStepSize(new QLineEdit("0.0001")),
- 			  _visStepSize(new QLineEdit("0.1"))
+        SimSettingDialogFMU::SimSettingDialogFMU(QWidget* parent)
+                : QDialog(parent),
+                  _solverBox(new QComboBox()),
+                  _simStepSizeLineEdit(new QLineEdit("0.0001")),
+                  _visStepSizeLineEdit(new QLineEdit("10"))
         {
-            // Perspective
-			_solverMethodBox->addItem(QString("forward euler"));
-
-            QLabel* solverMethodLabel = new QLabel(tr("Solver Method: "));
-            QHBoxLayout* solverLayout = new QHBoxLayout();
-			solverLayout->addWidget(solverMethodLabel);
-			solverLayout->addWidget(_solverMethodBox.get());
-
-            QHBoxLayout* simStepLayout = new QHBoxLayout();
-			QLabel* simStepLabel = new QLabel(tr("Simulation Step Size: "));
-			simStepLayout->addWidget(simStepLabel);
-			simStepLayout->addWidget(_simStepSize.get());
-			QHBoxLayout* visStepLayout = new QHBoxLayout();
-			QLabel* visStepLabel = new QLabel(tr("Visualization Step Size: "));
-			visStepLayout->addWidget(visStepLabel);
-			visStepLayout->addWidget(_visStepSize.get());
+            QDialogButtonBox* buttonBox = new QDialogButtonBox(QDialogButtonBox::Ok | QDialogButtonBox::Cancel);
+            QObject::connect(buttonBox, SIGNAL(accepted()), this, SLOT(accept()));
+            QObject::connect(buttonBox, SIGNAL(rejected()), this, SLOT(reject()));
+            QVBoxLayout* buttonsLayout = new QVBoxLayout();
+            buttonsLayout->addStretch(1);
+            buttonsLayout->addWidget(buttonBox);
 
             // Main layout
             QVBoxLayout* mainLayout = new QVBoxLayout();
-            mainLayout->addLayout(solverLayout);
-            mainLayout->addLayout(simStepLayout);
-			mainLayout->addLayout(visStepLayout);
             mainLayout->addStretch(1);
             setLayout(mainLayout);
             setWindowTitle(tr("Simulation Settings"));
+
+            // Solver method
+            _solverBox->addItem(QString("Forward Euler"));
+            QLabel* solverLabel = new QLabel(tr("Solver Method: "));
+            QHBoxLayout* solverLayout = new QHBoxLayout();
+            solverLayout->addWidget(solverLabel);
+            solverLayout->addWidget(_solverBox);
+
+            // Step size
+            QHBoxLayout* simStepSizeLayout = new QHBoxLayout();
+            QLabel* simStepSizeLabel = new QLabel(tr("Simulation Step Size (h): "));
+            simStepSizeLayout->addWidget(simStepSizeLabel);
+            simStepSizeLayout->addWidget(_simStepSizeLineEdit);
+
+            // Visualization step size, aka render frequency
+            QHBoxLayout* visStepSizeLayout = new QHBoxLayout();
+            QLabel* visStepSizeLabel = new QLabel(tr("Visualization Step Size (msec): "));
+            visStepSizeLayout->addWidget(visStepSizeLabel);
+            visStepSizeLayout->addWidget(_visStepSizeLineEdit);
+
+            mainLayout->addLayout(solverLayout);
+            mainLayout->addLayout(simStepSizeLayout);
+            mainLayout->addLayout(visStepSizeLayout);
+            mainLayout->addLayout(buttonsLayout);
+        }
+
+        void SimSettingDialogFMU::accept()
+        {
+            _simSet.solver = static_cast<Solver>(_solverBox->currentIndex());
+            _simSet.simStepSize = _simStepSizeLineEdit->text().toFloat();
+            _simSet.visStepSize = _visStepSizeLineEdit->text().toFloat();
+            QDialog::accept();
+        }
+
+        SimSettingsFMU SimSettingDialogFMU::getSimSettings() const
+        {
+            return _simSet;
+        }
+
+        SimSettingDialogMAT::SimSettingDialogMAT(QWidget* parent)
+                : QDialog(parent)
+        {
+            QDialogButtonBox* buttonBox = new QDialogButtonBox(QDialogButtonBox::Ok | QDialogButtonBox::Cancel);
+            QObject::connect(buttonBox, SIGNAL(accepted()), this, SLOT(accept()));
+            QObject::connect(buttonBox, SIGNAL(rejected()), this, SLOT(reject()));
+            QVBoxLayout* buttonsLayout = new QVBoxLayout();
+            buttonsLayout->addStretch(1);
+            buttonsLayout->addWidget(buttonBox);
+
+            // Main layout
+            QVBoxLayout* mainLayout = new QVBoxLayout();
+            mainLayout->addStretch(1);
+            setLayout(mainLayout);
+            setWindowTitle(tr("Simulation Settings"));
+
+            // Speed up or slow down of visualization
+            QLineEdit* speedUp = new QLineEdit("1");
+            speedUp->setValidator(new QDoubleValidator(0, 10, 2, this));
+
+            //                int* l = new int();
+            //                int* t = new int();
+            //                int* r = new int();
+            //                int* b = new int();
+            //                //speedUp->getTextMargins(int &l, int &t, int &r, int &b);
+            //                speedUp->getTextMargins(l, t, r, b);
+            //                std::cout << "l= " << *l << std::endl;
+            //                std::cout << "r= " << *l << std::endl;
+            //                std::cout << "t= " << *l << std::endl;
+            //                std::cout << "b= " << *l << std::endl;
+
+            QHBoxLayout* speedUpLayout = new QHBoxLayout();
+            QLabel* speedUpLabel = new QLabel(tr("Speed Up: "));
+            speedUpLayout->addWidget(speedUpLabel);
+            speedUpLayout->addWidget(speedUp);
+
+            mainLayout->addLayout(speedUpLayout);
+
+            mainLayout->addLayout(buttonsLayout);
+        }
+
+        void SimSettingDialogMAT::accept()
+        {
+            _simSet.speedup = _speedupLineEdit->text().toFloat();
+            QDialog::accept();
+        }
+        SimSettingsMAT SimSettingDialogMAT::getSimSettings() const
+        {
+            return _simSet;
         }
 
     }  // End namespace View
- }  // End namespace OMVIS
+}  // End namespace OMVIS
