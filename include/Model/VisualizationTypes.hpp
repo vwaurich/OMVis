e7d894b7ea4ac6431e886239d09586436c613427
mflehmig <martin.flehmig@tu-dresden.de>
2016-08-10 13:01:06 +0200

    Add visualization types as enum

---
+++ include/Model/VisualizationTypes.hpp
@@ -0,0 +1,29 @@
+/*
+ * VisualizationTyps.hpp
+ *
+ *  Created on: 04.08.2016
+ *      Author: mf
+ */
+
+#ifndef INCLUDE_MODEL_VISUALIZATIONTYPES_HPP_
+#define INCLUDE_MODEL_VISUALIZATIONTYPES_HPP_
+
+namespace OMVIS
+{
+    namespace Model
+    {
+
+        /*! This enum holds the possible visualization modes of OMVis. */
+        enum VisType
+        {
+            NONE = 0,
+            FMU = 1,
+            FMU_REMOTE = 2,
+            MAT = 3,
+            MAT_REMOTE = 4
+        };
+
+    }  // End namespace Model
+}  // End namespace OMVIS
+
+#endif /* INCLUDE_MODEL_VISUALIZATIONTYPES_HPP_ */
