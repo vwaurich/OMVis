e2ae6340a9ca766f716c819854e6eb9edf86cc2d
mflehmig <martin.flehmig@tu-dresden.de>
2016-08-11 15:38:52 +0200

    Add standard header and harmonize header guard

--- include/Model/VisualizationTypes.hpp
+++ include/Model/VisualizationTypes.hpp
@@ -1,12 +1,25 @@
 /*
- * VisualizationTyps.hpp
+ * Copyright (C) 2016, Volker Waurich
  *
- *  Created on: 04.08.2016
- *      Author: mf
+ * This file is part of OMVis.
+ *
+ * OMVis is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * OMVis is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with OMVis.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-#ifndef INCLUDE_MODEL_VISUALIZATIONTYPES_HPP_
-#define INCLUDE_MODEL_VISUALIZATIONTYPES_HPP_
+
+#ifndef INCLUDE_VISUALIZATIONTYPES_HPP_
+#define INCLUDE_VISUALIZATIONTYPES_HPP_
 
 namespace OMVIS
 {
@@ -26,4 +39,4 @@ namespace OMVIS
     }  // End namespace Model
 }  // End namespace OMVIS
 
-#endif /* INCLUDE_MODEL_VISUALIZATIONTYPES_HPP_ */
+#endif /* INCLUDE_VISUALIZATIONTYPES_HPP_ */
