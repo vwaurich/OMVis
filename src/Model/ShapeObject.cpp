7c27f13f9e764f544dc908a22f82babd86788311
mflehmig <martin.flehmig@tu-dresden.de>
2016-11-03 13:20:47 +0100

    Make initialization of members lot more clear and quick hack nice dumping

--- src/Model/ShapeObject.cpp
+++ src/Model/ShapeObject.cpp
@@ -34,20 +34,23 @@ namespace OMVIS
         ShapeObject::ShapeObject()
                 : _id("noID"),
                   _type("box"),
-			      _fileName("noFile"),
-                  _length(ShapeObjectAttribute(0.1)),
-                  _width(ShapeObjectAttribute(0.1)),
-                  _height(ShapeObjectAttribute(0.1)),
-                  _r{ShapeObjectAttribute(0.1), ShapeObjectAttribute(0.1), ShapeObjectAttribute(0.1)},
-                  _rShape{ShapeObjectAttribute(0.0), ShapeObjectAttribute(0.0), ShapeObjectAttribute(0.0)},
-                  _lDir{ShapeObjectAttribute(1.0), ShapeObjectAttribute(0.0), ShapeObjectAttribute(0.0)},
-                  _wDir{ShapeObjectAttribute(0.0), ShapeObjectAttribute(1.0), ShapeObjectAttribute(0.0)},
-                  _color{ShapeObjectAttribute(255.0), ShapeObjectAttribute(255.0), ShapeObjectAttribute(255.0)},
-                  _T{ShapeObjectAttribute(0.0), ShapeObjectAttribute(0.0), ShapeObjectAttribute(1.0),
-                     ShapeObjectAttribute(1.0), ShapeObjectAttribute(0.0), ShapeObjectAttribute(0.0),
-                     ShapeObjectAttribute(0.0), ShapeObjectAttribute(1.0), ShapeObjectAttribute(0.0)},
-                  _specCoeff(ShapeObjectAttribute(0.7)),
-                  _mat(osg::Matrix(0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0)),
+                  _fileName("noFile"),
+                  _length(0.1),
+                  _width(0.1),
+                  _height(0.1),
+                  _r { 0.1, 0.1, 0.1 },
+                  _rShape { 0.0, 0.0, 0.0 },
+                  _lDir { 1.0, 0.0, 0.0 },
+                  _wDir { 0.0, 1.0, 0.0 },
+                  _color { 255.0, 255.0, 255.0 },
+                  _T { 0.0, 0.0, 1.0,
+                       1.0, 0.0, 0.0,
+                       0.0, 1.0, 0.0 },
+                  _specCoeff(0.7),
+                  _mat(0.0, 0.0, 0.0, 0.0,
+                       0.0, 0.0, 0.0, 0.0,
+                       0.0, 0.0, 0.0, 0.0,
+                       0.0, 0.0, 0.0, 0.0),
                   _extra(0.0)
         {
         }
@@ -58,26 +61,35 @@ namespace OMVIS
 
         void ShapeObject::dumpVisAttributes() const
         {
-            std::cout << "id " << _id << std::endl;
-            std::cout << "type " << _type << std::endl;
-			std::cout << "fileName " << _fileName << std::endl;
-            std::cout << "length " << _length.getValueString() << std::endl;
-            std::cout << "width " << _width.getValueString() << std::endl;
-            std::cout << "height " << _height.getValueString() << std::endl;
-            std::cout << "lDir " << _lDir[0].getValueString() << ", " << _lDir[1].getValueString() << ", " << _lDir[2].getValueString() << ", " << std::endl;
-            std::cout << "wDir " << _wDir[0].getValueString() << ", " << _wDir[1].getValueString() << ", " << _wDir[2].getValueString() << ", " << std::endl;
-            std::cout << "r " << _r[0].getValueString() << ", " << _r[1].getValueString() << ", " << _r[2].getValueString() << ", " << std::endl;
-            std::cout << "r_shape " << _rShape[0].getValueString() << ", " << _rShape[1].getValueString() << ", " << _rShape[2].getValueString() << ", " << std::endl;
-            std::cout << "T0 " << _T[0].getValueString() << ", " << _T[1].getValueString() << ", " << _T[2].getValueString() << ", " << std::endl;
-            std::cout << "   " << _T[3].getValueString() << ", " << _T[4].getValueString() << ", " << _T[5].getValueString() << ", " << std::endl;
-            std::cout << "   " << _T[6].getValueString() << ", " << _T[7].getValueString() << ", " << _T[8].getValueString() << ", " << std::endl;
-            std::cout << "color " << _color[0].getValueString() << ", " << _color[1].getValueString() << ", " << _color[2].getValueString() << ", " << std::endl;
-            std::cout << "mat " << _mat(0, 0) << ", " << _mat(0, 1) << ", " << _mat(0, 2) << ", " << _mat(0, 3) << std::endl;
-            std::cout << "    " << _mat(1, 0) << ", " << _mat(1, 1) << ", " << _mat(1, 2) << ", " << _mat(1, 3) << std::endl;
-            std::cout << "    " << _mat(2, 0) << ", " << _mat(2, 1) << ", " << _mat(2, 2) << ", " << _mat(2, 3) << std::endl;
-            std::cout << "    " << _mat(3, 0) << ", " << _mat(3, 1) << ", " << _mat(3, 2) << ", " << _mat(3, 3) << std::endl;
-            std::cout << "extra " << _extra.getValueString() << std::endl;
+            std::cout << "Dumping VisAttributes ...\n";
+            std::cout << "  id: \t\t" << _id
+                      << "\n  type: \t" << _type
+                      << "\n  fileName: \t" << _fileName
+                      << "\n  length: \t" << _length.getValueString()
+                      << "\n  width: \t" << _width.getValueString()
+                      << "\n  height: \t" << _height.getValueString()
+                      << "\n  lDir: \t" << '[' << _lDir[0].getValueString() << ", " << _lDir[1].getValueString() << ", "
+                                   << _lDir[2].getValueString()  << ']'
+                      << "\n  wDir: \t" << '[' << _wDir[0].getValueString() << ", " << _wDir[1].getValueString() << ", "
+                                   << _wDir[2].getValueString()  << ']'
+                      << "\n  r: \t\t" << '[' <<_r[0].getValueString() << ", " << _r[1].getValueString() << ", "
+                                   << _r[2].getValueString()  << ']'
+                      << "\n  r_shape: \t" <<  '[' <<_rShape[0].getValueString() << ", " << _rShape[1].getValueString() << ", "
+                                         << _rShape[2].getValueString() << ']'
+                      << "\n  T0: \t\t" << '[' << _T[0].getValueString() << ", " << _T[1].getValueString() << ", "
+                                 << _T[2].getValueString() << ", "
+                      << "\n \t\t " << _T[3].getValueString() << ", " << _T[4].getValueString() << ", "
+                               << _T[5].getValueString() << ", "
+                      << "\n \t\t " << _T[6].getValueString() << ", " << _T[7].getValueString() << ", "
+                               << _T[8].getValueString() << ']'
+                      << "\n  color: \t" << '[' << _color[0].getValueString() << ", " << _color[1].getValueString() << ", "
+                                    << _color[2].getValueString() << ']'
+                      << "\n  mat: \t\t" << '[' << _mat(0, 0) << ", " << _mat(0, 1) << ", " << _mat(0, 2) << ", " << _mat(0, 3)
+                      << "    " << _mat(1, 0) << ", " << _mat(1, 1) << ", " << _mat(1, 2) << ", " << _mat(1, 3)
+                      << "    " << _mat(2, 0) << ", " << _mat(2, 1) << ", " << _mat(2, 2) << ", " << _mat(2, 3)
+                      << "    " << _mat(3, 0) << ", " << _mat(3, 1) << ", " << _mat(3, 2) << ", " << _mat(3, 3) << ']'
+                      << "\n  extra: \t" << _extra.getValueString() << std::endl;
         }
 
-    } // End namespace Model
-} // End namespace OMVis
+    }  // End namespace Model
+}  // namespace OMVIS
