03e012f814f414b2f46c5e600d42b873c9b9946f
mflehmig <martin.flehmig@tu-dresden.de>
2016-11-03 10:04:04 +0100

    Modernize and adopt to code style

--- src/Control/GUIController.cpp
+++ src/Control/GUIController.cpp
@@ -17,17 +17,17 @@
  * along with OMVis.  If not, see <http://www.gnu.org/licenses/>.
  */
 
+#include "Control/GUIController.hpp"
+#include "Control/TimeManager.hpp"
 #include "Model/VisualizerFMU.hpp"
 #include "Model/VisualizerMAT.hpp"
 #include "Model/VisualizerFMUClient.hpp"
-#include "Control/TimeManager.hpp"
 #include "Initialization/Factory.hpp"
-#include "Control/GUIController.hpp"
 #include "Util/Logger.hpp"
 #include "Util/Util.hpp"
 
-#include <sys/stat.h>
 #include <stdexcept>
+#include <sys/stat.h>
 
 namespace OMVIS
 {
@@ -47,8 +47,8 @@ namespace OMVIS
          * INITIALIZATION METHODS
          *---------------------------------------*/
 
-        void GUIController::loadModel(const Initialization::VisualizationConstructionPlan& cP, const int timeSliderStart,
-                                      const int timeSliderEnd)
+        void GUIController::loadModel(const Initialization::VisualizationConstructionPlan& cP,
+                                      const int timeSliderStart, const int timeSliderEnd)
         {
             LOGGER_WRITE(std::string("GUIController::loadModel()"), Util::LC_CTR, Util::LL_DEBUG);
 
@@ -56,7 +56,8 @@ namespace OMVIS
             bool xmlExists = Util::checkForXMLFile(cP.modelFile, cP.path);
             if (!xmlExists)
             {
-                std::string msg = "Could not find the visual XML file "+ Util::getXMLFileName(cP.modelFile, cP.path) +" in the model file directory "+ cP.path;
+                auto msg = "Could not find the visual XML file " + Util::getXMLFileName(cP.modelFile, cP.path)
+                        + " in the model file directory " + cP.path;
                 LOGGER_WRITE(msg, Util::LC_LOADER, Util::LL_ERROR);
                 throw std::runtime_error(msg);
             }
@@ -64,13 +65,14 @@ namespace OMVIS
             // Some useful output for the user and developer.
             LOGGER_WRITE(std::string("Path to model: ") + cP.path, Util::LC_CTR, Util::LL_DEBUG);
             LOGGER_WRITE(std::string("Model file: ") + cP.modelFile, Util::LC_CTR, Util::LL_DEBUG);
-            LOGGER_WRITE(std::string("XML file exists: ") + Util::boolToString(xmlExists), Util::LC_CTR, Util::LL_DEBUG);
+            LOGGER_WRITE(std::string("XML file exists: ") + Util::boolToString(xmlExists), Util::LC_CTR,
+                         Util::LL_DEBUG);
 
             // Corner case: The chosen model is the very same that is already loaded. In case of FMUs this means
             // unpacking an already unpacked shared object, which leads to a segmentation fault. Thats why we test for
             // this case. If the model is already loaded, we initialize it again.
             if (modelIsLoaded() && cP.path == _modelVisualizer->getBaseData()->getPath()
-                                && cP.modelFile == _modelVisualizer->getBaseData()->getModelFile())
+                    && cP.modelFile == _modelVisualizer->getBaseData()->getModelFile())
             {
                 LOGGER_WRITE(std::string("You tried to load the same model that is already loaded in OMVis. "
                                          "The model will be initialized again."),
@@ -78,11 +80,13 @@ namespace OMVIS
                 initVisualization();
             }
             else
+            {
                 loadModelHelper(&cP, timeSliderStart, timeSliderEnd);
+            }
         }
 
-        void GUIController::loadModel(const Initialization::RemoteVisualizationConstructionPlan& cP, const int timeSliderStart,
-                                      const int timeSliderEnd)
+        void GUIController::loadModel(const Initialization::RemoteVisualizationConstructionPlan& cP,
+                                      const int timeSliderStart, const int timeSliderEnd)
         {
             LOGGER_WRITE(std::string("GUIController::loadModel()"), Util::LC_CTR, Util::LL_DEBUG);
 
@@ -90,7 +94,7 @@ namespace OMVIS
             bool xmlExists = Util::checkForXMLFile(cP.modelFile, cP.wDir);
             if (!xmlExists)
             {
-                std::string msg = "Visual XML file does not exist on localhost in the working directory " + cP.wDir + ".\n";
+                auto msg = "Visual XML file does not exist on localhost in the working directory " + cP.wDir + ".\n";
                 LOGGER_WRITE(msg, Util::LC_LOADER, Util::LL_ERROR);
                 throw std::runtime_error(msg);
             }
@@ -101,13 +105,14 @@ namespace OMVIS
             LOGGER_WRITE(std::string("Path of model file: ") + cP.path, Util::LC_CTR, Util::LL_DEBUG);
             LOGGER_WRITE(std::string("Port number: ") + std::to_string(cP.port), Util::LC_CTR, Util::LL_DEBUG);
             LOGGER_WRITE(std::string("Local working directory: ") + cP.wDir, Util::LC_CTR, Util::LL_DEBUG);
-            LOGGER_WRITE(std::string("XML file exists: ") + Util::boolToString(xmlExists), Util::LC_CTR, Util::LL_DEBUG);
+            LOGGER_WRITE(std::string("XML file exists: ") + Util::boolToString(xmlExists), Util::LC_CTR,
+                         Util::LL_DEBUG);
 
             // Corner case: The chosen model is the very same that is already loaded. In case of FMUs this means
             // unpacking an already unpacked shared object, which leads to a segmentation fault. Thats why we test for
             // this case. If the model is already loaded, we initialize it again.
             if (modelIsLoaded() && cP.wDir == _modelVisualizer->getBaseData()->getPath()
-                                && cP.modelFile == _modelVisualizer->getBaseData()->getModelFile())
+                    && cP.modelFile == _modelVisualizer->getBaseData()->getModelFile())
             {
                 LOGGER_WRITE(std::string("You tried to load the same model that is already loaded in OMVis. "
                                          "The model will be initialized again."),
@@ -115,21 +120,23 @@ namespace OMVIS
                 initVisualization();
             }
             else
-                loadModelHelper(dynamic_cast<const Initialization::VisualizationConstructionPlan*>(&cP), timeSliderStart,
-                                timeSliderEnd);
+            {
+                loadModelHelper(dynamic_cast<const Initialization::VisualizationConstructionPlan*>(&cP),
+                                timeSliderStart, timeSliderEnd);
+            }
         }
 
-        void GUIController::loadModelHelper(const Initialization::VisualizationConstructionPlan* cP, const int timeSliderStart,
-                                            const int timeSliderEnd)
+        void GUIController::loadModelHelper(const Initialization::VisualizationConstructionPlan* cP,
+                                            const int timeSliderStart, const int timeSliderEnd)
         {
             // Okay, do we already have a model loaded? If so, we keep this loaded model in case of the new model
             // cannot be loaded. If everything went fine, we take the loaded model.
 
             // Ask the factory to create an appropriate Visualizer object.
-            Initialization::Factory* factory = new Initialization::Factory();
-            std::shared_ptr<Model::VisualizerAbstract> tmpVisualizer = factory->createVisualizerObject(cP);
+            auto factory = new Initialization::Factory();
+            auto tmpVisualizer = factory->createVisualizerObject(cP);
 
-            if (tmpVisualizer != nullptr)
+            if (nullptr != tmpVisualizer)
             {
                 tmpVisualizer->getTimeManager()->setSliderRange(timeSliderStart, timeSliderEnd);
 
@@ -138,7 +145,7 @@ namespace OMVIS
             }
             else
             {
-                std::string msg = "Could not load model. Factory returned nullptr.";
+                auto msg = "Could not load model. Factory returned nullptr.";
                 LOGGER_WRITE(msg, Util::LC_LOADER, Util::LL_ERROR);
                 throw std::runtime_error(msg);
             }
@@ -160,22 +167,28 @@ namespace OMVIS
 
         void GUIController::startVisualization()
         {
-            if (_modelVisualizer != nullptr)
+            if (nullptr != _modelVisualizer)
+            {
                 _modelVisualizer->startVisualization();
+            }
         }
 
         void GUIController::pauseVisualization()
         {
-            if (_modelVisualizer != nullptr)
+            if (nullptr != _modelVisualizer)
+            {
                 _modelVisualizer->pauseVisualization();
+            }
         }
 
         void GUIController::initVisualization()
         {
             // Todo: Check if input variables need to be specified in order to interact with the FMU. If so,
             // open a Gui.
-            if (_modelVisualizer != nullptr)
+            if (nullptr != _modelVisualizer)
+            {
                 _modelVisualizer->initVisualization();
+            }
         }
 
         void GUIController::sceneUpdate()
@@ -199,9 +212,9 @@ namespace OMVIS
 
         void GUIController::setVisTime(const int val)
         {
-            _modelVisualizer->getTimeManager()->setVisTime((_modelVisualizer->getTimeManager()->getEndTime()
-                                                            - _modelVisualizer->getTimeManager()->getStartTime())
-                                                            * (float) (val / 100.0));
+            _modelVisualizer->getTimeManager()->setVisTime(
+                    (_modelVisualizer->getTimeManager()->getEndTime()
+                            - _modelVisualizer->getTimeManager()->getStartTime()) * static_cast<float>(val / 100.0));
         }
 
         double GUIController::getVisTime()
@@ -226,7 +239,7 @@ namespace OMVIS
 
         bool GUIController::visTypeIs(const Model::VisType visType) const
         {
-            return (_modelVisualizer->getVisType() == visType) ? true : false;
+            return _modelVisualizer->getVisType() == visType;
         }
 
         bool GUIController::visTypeIsNone() const
@@ -261,8 +274,8 @@ namespace OMVIS
 
         Model::UserSimSettingsFMU GUIController::getCurrentSimSettings() const
         {
-          std::shared_ptr<Model::VisualizerFMU> omVisFMU = std::dynamic_pointer_cast<Model::VisualizerFMU>(_modelVisualizer);
-          return omVisFMU->getCurrentSimSettings();
+            auto omVisFMU = std::dynamic_pointer_cast<Model::VisualizerFMU>(_modelVisualizer);
+            return omVisFMU->getCurrentSimSettings();
         }
 
         bool GUIController::modelIsLoaded()
@@ -274,18 +287,17 @@ namespace OMVIS
         {
             if (visTypeIsFMU())
             {
-                std::shared_ptr<Model::VisualizerFMU> omVisFMU =
-                                                     std::dynamic_pointer_cast<Model::VisualizerFMU>(_modelVisualizer);
+                auto omVisFMU = std::dynamic_pointer_cast<Model::VisualizerFMU>(_modelVisualizer);
                 return omVisFMU->getInputData();
             }
-            else if (visTypeIsFMURemote())
+            if (visTypeIsFMURemote())
             {
-                std::shared_ptr<Model::VisualizerFMUClient> omVisFMU =
-                                               std::dynamic_pointer_cast<Model::VisualizerFMUClient>(_modelVisualizer);
+                auto omVisFMU = std::dynamic_pointer_cast<Model::VisualizerFMUClient>(_modelVisualizer);
                 return omVisFMU->getInputData();
             }
-            else
-                return nullptr;
+
+            // Else
+            return nullptr;
         }
 
         void GUIController::handleSimulationSettings(const Model::UserSimSettingsFMU& simSetFMU)
@@ -296,9 +308,13 @@ namespace OMVIS
                 initVisualization();
             }
             else if (visTypeIsFMURemote())
+            {
                 LOGGER_WRITE("Not yet implemented for FMU Remote visualization.", Util::LC_CTR, Util::LL_INFO);
+            }
             else
+            {
                 throw std::runtime_error("Wrong function called");
+            }
         }
 
         void GUIController::handleSimulationSettings(const Model::UserSimSettingsMAT& simSetMAT)
@@ -309,10 +325,14 @@ namespace OMVIS
                 //initVisualization();
             }
             else if (visTypeIsMATRemote())
+            {
                 LOGGER_WRITE("Not yet implemented for MAT Remote visualization.", Util::LC_CTR, Util::LL_INFO);
+            }
             else
+            {
                 throw std::runtime_error("Wrong function called");
+            }
         }
 
-    }  // End namespace Control
-}  // End namespace OMVIS
+    }  // namespace Control
+}  // namespace OMVIS
