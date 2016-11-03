4b352c513739d9f7ee3fd52f1c6f44beace882b9
mflehmig <martin.flehmig@tu-dresden.de>
2016-11-03 14:29:43 +0100

    Modernize and adopt to code style

--- src/Model/VisualizerAbstract.cpp
+++ src/Model/VisualizerAbstract.cpp
@@ -17,13 +17,13 @@
  * along with OMVis.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-#include "Util/Logger.hpp"
 #include "Model/VisualizerAbstract.hpp"
+#include "Util/Logger.hpp"
 
 #include <boost/filesystem.hpp>
 
-#include <string>
 #include <stdlib.h>
+#include <string>
 
 namespace OMVIS
 {
@@ -43,22 +43,23 @@ namespace OMVIS
         {
         }
 
-        VisualizerAbstract::VisualizerAbstract(const std::string& modelFile, const std::string& path, const VisType visType)
+        VisualizerAbstract::VisualizerAbstract(const std::string& modelFile, const std::string& path,
+                                               const VisType visType)
                 : _visType(visType),
                   _baseData(nullptr),
-                  _viewerStuff(new OMVisScene()),
-                  _nodeUpdater(new Model::UpdateVisitor()),
-                  _timeManager(new Control::TimeManager(0.0, 0.0, 0.0, 0.0, 0.1, 0.0, 100.0))
+                  _viewerStuff(std::make_shared<OMVisScene>()),
+                  _nodeUpdater(std::make_shared<Model::UpdateVisitor>()),
+                  _timeManager(std::make_shared<Control::TimeManager>(0.0, 0.0, 0.0, 0.0, 0.1, 0.0, 100.0))
         {
             // We need the absolute path to the directory. Otherwise the FMUlibrary can not open the shared objects.
             //char fullPathTmp[PATH_MAX];
             //realpath(path.c_str(), fullPathTmp);
-            boost::filesystem::path bPath = boost::filesystem::path(path.c_str());
+            auto bPath = boost::filesystem::path(path.c_str());
             bPath = boost::filesystem::absolute(bPath);
             std::string fullPath = bPath.string();
 
             // Now we can use the full path.
-            _baseData = std::shared_ptr<OMVisualBase>(new OMVisualBase(modelFile, fullPath));
+            _baseData = std::make_shared<OMVisualBase>(modelFile, fullPath);
             _viewerStuff->getScene()->setPath(fullPath);
         }
 
@@ -87,7 +88,7 @@ namespace OMVIS
         {
             // Build scene graph.
             LOGGER_WRITE(std::string("Setup scene for ") + std::to_string(_baseData->_shapes.size()) + " shapes.",
-                                                                                     Util::LC_LOADER, Util::LL_DEBUG);
+                         Util::LC_LOADER, Util::LL_DEBUG);
             _viewerStuff->getScene()->setUpScene(_baseData->_shapes);
         }
 
@@ -132,14 +133,18 @@ namespace OMVIS
                 LOGGER_WRITE(std::string("Start visualization ..."), Util::LC_CTR, Util::LL_INFO);
             }
             else
+            {
                 LOGGER_WRITE(std::string("There is nothing left to visualize. Initialize the model first."),
                              Util::LC_CTR, Util::LL_INFO);
+            }
         }
 
         void VisualizerAbstract::pauseVisualization()
         {
-            LOGGER_WRITE(std::string("Pause visualization at ") + std::to_string(_timeManager->getVisTime())
-                         + std::string("."), Util::LC_CTR, Util::LL_INFO);
+            LOGGER_WRITE(
+                    std::string("Pause visualization at ") + std::to_string(_timeManager->getVisTime())
+                            + std::string("."),
+                    Util::LC_CTR, Util::LL_INFO);
             _timeManager->setPause(true);
         }
 
@@ -161,10 +166,11 @@ namespace OMVIS
                 updateScene(_timeManager->getVisTime());
                 _timeManager->setVisTime(_timeManager->getVisTime() + _timeManager->getHVisual());
 
-                LOGGER_WRITE(std::string("Update scene at ") + std::to_string(_timeManager->getVisTime())
-                             + std::string(" simTime ") + std::to_string(_timeManager->getSimTime())
-                             + std::string(" _visStepSize ") + std::to_string(_timeManager->getHVisual()),
-                             Util::LC_CTR, Util::LL_INFO);
+                LOGGER_WRITE(
+                        std::string("Update scene at ") + std::to_string(_timeManager->getVisTime())
+                                + std::string(" simTime ") + std::to_string(_timeManager->getSimTime())
+                                + std::string(" _visStepSize ") + std::to_string(_timeManager->getHVisual()),
+                        Util::LC_CTR, Util::LL_INFO);
                 if (_timeManager->getVisTime() >= _timeManager->getEndTime() - 1.e-6)
                 {
                     LOGGER_WRITE(std::string("The End."), Util::LC_CTR, Util::LL_INFO);
@@ -173,5 +179,5 @@ namespace OMVIS
             }
         }
 
-    }  // End namespace Model
-}  // End namespace OMVIS
+    }  // namespace Model
+}  // namespace OMVIS
