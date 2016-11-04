650eaa19b5f843b3bfcad37e9ef2ac1a6e468cc2
mflehmig <martin.flehmig@tu-dresden.de>
2016-11-04 07:47:50 +0100

    Modernize

--- src/Model/FMUWrapper.cpp
+++ src/Model/FMUWrapper.cpp
@@ -53,14 +53,10 @@ namespace OMVIS
         FMUWrapper::~FMUWrapper()
         {
             // Free memory associated with the FMUData and its context.
-            if (_fmuData._states)
-                delete (_fmuData._states);
-            if (_fmuData._statesDer)
-                delete (_fmuData._statesDer);
-            if (_fmuData._eventIndicators)
-                delete (_fmuData._eventIndicators);
-            if (_fmuData._eventIndicatorsPrev)
-                delete (_fmuData._eventIndicatorsPrev);
+            delete _fmuData._states;
+            delete _fmuData._statesDer;
+            delete _fmuData._eventIndicators;
+            delete _fmuData._eventIndicatorsPrev;
         }
 
         /*-----------------------------------------
@@ -76,7 +72,7 @@ namespace OMVIS
             _callbacks.free = free;
             _callbacks.logger = jm_default_logger;
             _callbacks.log_level = jm_log_level_debug;  // jm_log_level_error;
-            _callbacks.context = 0;
+            _callbacks.context = nullptr;
 
             _callBackFunctions.logger = fmi1_log_forwarding;
             _callBackFunctions.allocateMemory = calloc;
@@ -86,42 +82,54 @@ namespace OMVIS
             //printf("Library build stamp:\n%s\n", fmilib_get_build_stamp());
             std::cout << "Library build stamp: \n" << fmilib_get_build_stamp() << std::endl;
 #endif
-            _context = std::shared_ptr<fmi_import_context_t>(fmi_import_allocate_context(&_callbacks), fmi_import_free_context);
+            _context = std::shared_ptr<fmi_import_context_t>(fmi_import_allocate_context(&_callbacks),
+                                                             fmi_import_free_context);
 
             // If the FMU is already extracted, we remove the shared object file.
             std::string sharedObjectFile(fmi_import_get_dll_path(path.c_str(), modelFile.c_str(), &_callbacks));
             if (Util::fileExists(sharedObjectFile))
             {
-                if (remove(sharedObjectFile.c_str()) != 0)
-                    LOGGER_WRITE(std::string("Error deleting the shared object file " + sharedObjectFile + std::string(".")), Util::LC_LOADER, Util::LL_ERROR);
+                if (0 != remove(sharedObjectFile.c_str()))
+                {
+                    LOGGER_WRITE("Error deleting the shared object file " + sharedObjectFile + ".", Util::LC_LOADER,
+                                 Util::LL_ERROR);
+                }
                 else
-                    LOGGER_WRITE(std::string("Shared object file " + sharedObjectFile + std::string(" deleted.")), Util::LC_LOADER, Util::LL_DEBUG);
+                {
+                    LOGGER_WRITE("Shared object file " + sharedObjectFile + " deleted.", Util::LC_LOADER,
+                                 Util::LL_DEBUG);
+                }
             }
             else
-                LOGGER_WRITE(std::string("Shared object file " + sharedObjectFile + std::string(" does not exist.")), Util::LC_LOADER, Util::LL_DEBUG);
+            {
+                LOGGER_WRITE("Shared object file " + sharedObjectFile + " does not exist.", Util::LC_LOADER,
+                             Util::LL_DEBUG);
+            }
 
             // Unzip the FMU and pars it.
             // Unzip the FMU only once. Overwriting the dll/so file may cause a segmentation fault.
             std::string fmuFileName = path + modelFile;
             fmi_version_enu_t version = fmi_import_get_fmi_version(_context.get(), fmuFileName.c_str(), path.c_str());
-            if (version != fmi_version_1_enu)
+            if (fmi_version_1_enu != version)
             {
-                LOGGER_WRITE(std::string("Only version 1.0 is supported so far. Exiting."), Util::LC_LOADER, Util::LL_ERROR);
+                LOGGER_WRITE("Only version 1.0 is supported so far. Exiting.", Util::LC_LOADER, Util::LL_ERROR);
                 doExit();
             }
 
-            _fmu = std::shared_ptr<fmi1_import_t>(fmi1_import_parse_xml(_context.get(), path.c_str()), fmi1_import_free);
+            _fmu = std::shared_ptr<fmi1_import_t>(fmi1_import_parse_xml(_context.get(), path.c_str()),
+                                                  fmi1_import_free);
             if (!_fmu)
             {
-                LOGGER_WRITE(std::string("Error parsing XML. Exiting."), Util::LC_LOADER, Util::LL_ERROR);
+                LOGGER_WRITE("Error parsing XML. Exiting.", Util::LC_LOADER, Util::LL_ERROR);
                 doExit();
             }
 
             //loadFMU dll
             jm_status_enu_t status = fmi1_import_create_dllfmu(_fmu.get(), _callBackFunctions, 1);
-            if (status == jm_status_error)
+            if (jm_status_error == status)
             {
-                LOGGER_WRITE(std::string("Could not create the DLL loading mechanism(C-API test). Exiting."), Util::LC_LOADER, Util::LL_ERROR);
+                LOGGER_WRITE("Could not create the DLL loading mechanism(C-API test). Exiting.", Util::LC_LOADER,
+                             Util::LL_ERROR);
                 doExit();
             }
         }
@@ -132,24 +140,30 @@ namespace OMVIS
             _fmuData._hcur = simSettings->getHdef();
             _fmuData._tcur = simSettings->getTstart();
 
-            LOGGER_WRITE(std::string("Version returned from FMU: ") + std::string(fmi1_import_get_version(_fmu.get())), Util::LC_LOADER, Util::LL_INFO);
-            LOGGER_WRITE(std::string("Platform type returned: ") + std::string(fmi1_import_get_model_types_platform(_fmu.get())), Util::LC_LOADER, Util::LL_INFO);
+            LOGGER_WRITE("Version returned from FMU: " + std::string(fmi1_import_get_version(_fmu.get())),
+                         Util::LC_LOADER, Util::LL_INFO);
+            LOGGER_WRITE("Platform type returned: " + std::string(fmi1_import_get_model_types_platform(_fmu.get())),
+                         Util::LC_LOADER, Util::LL_INFO);
 
             // Calloc everything
             _fmuData._nStates = fmi1_import_get_number_of_continuous_states(_fmu.get());
             _fmuData._nEventIndicators = fmi1_import_get_number_of_event_indicators(_fmu.get());
-            LOGGER_WRITE(std::string("n_states: ") + std::to_string(_fmuData._nStates) + " " + std::to_string(_fmuData._nEventIndicators), Util::LC_LOADER, Util::LL_INFO);
+            LOGGER_WRITE(
+                    "n_states: " + std::to_string(_fmuData._nStates) + " " + std::to_string(_fmuData._nEventIndicators),
+                    Util::LC_LOADER, Util::LL_INFO);
 
-            _fmuData._states = (fmi1_real_t*) calloc(_fmuData._nStates, sizeof(double));
-            _fmuData._statesDer = (fmi1_real_t*) calloc(_fmuData._nStates, sizeof(double));
-            _fmuData._eventIndicators = (fmi1_real_t*) calloc(_fmuData._nEventIndicators, sizeof(double));
-            _fmuData._eventIndicatorsPrev = (fmi1_real_t*) calloc(_fmuData._nEventIndicators, sizeof(double));
+            _fmuData._states = reinterpret_cast<fmi1_real_t*>(calloc(_fmuData._nStates, sizeof(double)));
+            _fmuData._statesDer = reinterpret_cast<fmi1_real_t*>(calloc(_fmuData._nStates, sizeof(double)));
+            _fmuData._eventIndicators = reinterpret_cast<fmi1_real_t*>(calloc(_fmuData._nEventIndicators,
+                                                                              sizeof(double)));
+            _fmuData._eventIndicatorsPrev = reinterpret_cast<fmi1_real_t*>(calloc(_fmuData._nEventIndicators,
+                                                                                  sizeof(double)));
 
             // Instantiate model
             jm_status_enu_t jmstatus = fmi1_import_instantiate_model(_fmu.get(), "Test ME model instance");
-            if (jmstatus == jm_status_error)
+            if (jm_status_error == jmstatus)
             {
-                LOGGER_WRITE(std::string("fmi1_import_instantiate_model failed. Exiting."), Util::LC_LOADER, Util::LL_ERROR);
+                LOGGER_WRITE("fmi1_import_instantiate_model failed. Exiting.", Util::LC_LOADER, Util::LL_ERROR);
                 doExit();
             }
 
@@ -157,20 +171,22 @@ namespace OMVIS
             _fmuData._fmiStatus = fmi1_import_set_time(_fmu.get(), simSettings->getTstart());
             try
             {
-                _fmuData._fmiStatus = fmi1_import_initialize(_fmu.get(), simSettings->getToleranceControlled(), simSettings->getRelativeTolerance(), &_fmuData._eventInfo);
+                _fmuData._fmiStatus = fmi1_import_initialize(_fmu.get(), simSettings->getToleranceControlled(),
+                                                             simSettings->getRelativeTolerance(), &_fmuData._eventInfo);
             }
             catch (std::exception &ex)
             {
                 std::cout << __FILE__ << " : " << __LINE__ << " Exception: " << ex.what() << std::endl;
             }
             _fmuData._fmiStatus = fmi1_import_get_continuous_states(_fmu.get(), _fmuData._states, _fmuData._nStates);
-            _fmuData._fmiStatus = fmi1_import_get_event_indicators(_fmu.get(), _fmuData._eventIndicatorsPrev, _fmuData._nEventIndicators);
+            _fmuData._fmiStatus = fmi1_import_get_event_indicators(_fmu.get(), _fmuData._eventIndicatorsPrev,
+                                                                   _fmuData._nEventIndicators);
             _fmuData._fmiStatus = fmi1_import_set_debug_logging(_fmu.get(), fmi1_false);
 
             // Turn on logging in FMI library.
             fmi1_import_set_debug_logging(_fmu.get(), fmi1_false);
 
-            LOGGER_WRITE(std::string("FMU::initialize(). Finished."), Util::LC_LOADER, Util::LL_INFO);
+            LOGGER_WRITE("FMU::initialize(). Finished.", Util::LC_LOADER, Util::LL_INFO);
         }
 
         /*-----------------------------------------
@@ -207,7 +223,7 @@ namespace OMVIS
             {
                 if (_fmuData._eventIndicators[k] * _fmuData._eventIndicatorsPrev[k] < 0)
                 {
-                    LOGGER_WRITE(std::string("Event occurred at ") + std::to_string(_fmuData._tcur), Util::LC_CTR, Util::LL_DEBUG);
+                    LOGGER_WRITE("Event occurred at " + std::to_string(_fmuData._tcur), Util::LC_CTR, Util::LL_DEBUG);
                     return true;
                 }
             }
@@ -216,20 +232,26 @@ namespace OMVIS
 
         bool FMUWrapper::itsEventTime() const
         {
-            return (_fmuData._eventInfo.upcomingTimeEvent && _fmuData._tcur == _fmuData._eventInfo.nextEventTime);
+            return ((0 != _fmuData._eventInfo.upcomingTimeEvent) && _fmuData._tcur == _fmuData._eventInfo.nextEventTime);
         }
 
         void FMUWrapper::updateNextTimeStep(const fmi1_real_t hdef)
         {
-            if (_fmuData._eventInfo.upcomingTimeEvent)
+            if (0 != _fmuData._eventInfo.upcomingTimeEvent)
             {
                 if (_fmuData._tcur + hdef < _fmuData._eventInfo.nextEventTime)
+                {
                     _fmuData._hcur = hdef;
+                }
                 else
+                {
                     _fmuData._hcur = _fmuData._eventInfo.nextEventTime - _fmuData._tcur;
+                }
             }
             else
+            {
                 _fmuData._hcur = hdef;
+            }
 
             // Increase with step size
             _fmuData._tcur += _fmuData._hcur;
@@ -242,17 +264,20 @@ namespace OMVIS
 
         void FMUWrapper::handleEvents(const fmi1_boolean_t intermediateResults)
         {
-            LOGGER_WRITE(std::string("Handle event at ") + std::to_string(_fmuData._tcur), Util::LC_CTR, Util::LL_DEBUG);
+            LOGGER_WRITE("Handle event at " + std::to_string(_fmuData._tcur), Util::LC_CTR, Util::LL_DEBUG);
             _fmuData._fmiStatus = fmi1_import_eventUpdate(_fmu.get(), intermediateResults, &_fmuData._eventInfo);
             _fmuData._fmiStatus = fmi1_import_get_continuous_states(_fmu.get(), _fmuData._states, _fmuData._nStates);
-            _fmuData._fmiStatus = fmi1_import_get_event_indicators(_fmu.get(), _fmuData._eventIndicators, _fmuData._nEventIndicators);
-            _fmuData._fmiStatus = fmi1_import_get_event_indicators(_fmu.get(), _fmuData._eventIndicatorsPrev, _fmuData._nEventIndicators);
+            _fmuData._fmiStatus = fmi1_import_get_event_indicators(_fmu.get(), _fmuData._eventIndicators,
+                                                                   _fmuData._nEventIndicators);
+            _fmuData._fmiStatus = fmi1_import_get_event_indicators(_fmu.get(), _fmuData._eventIndicatorsPrev,
+                                                                   _fmuData._nEventIndicators);
         }
 
         void FMUWrapper::prepareSimulationStep(const double time)
         {
             _fmuData._fmiStatus = fmi1_import_set_time(_fmu.get(), time);
-            _fmuData._fmiStatus = fmi1_import_get_event_indicators(_fmu.get(), _fmuData._eventIndicators, _fmuData._nEventIndicators);
+            _fmuData._fmiStatus = fmi1_import_get_event_indicators(_fmu.get(), _fmuData._eventIndicators,
+                                                                   _fmuData._nEventIndicators);
         }
 
         void FMUWrapper::updateTimes(const double simTimeEnd)
@@ -273,7 +298,9 @@ namespace OMVIS
         void FMUWrapper::doEulerStep()
         {
             for (size_t k = 0; k < _fmuData._nStates; ++k)
+            {
                 _fmuData._states[k] = _fmuData._states[k] + _fmuData._hcur * _fmuData._statesDer[k];
+            }
         }
 
         void FMUWrapper::completedIntegratorStep(fmi1_boolean_t* callEventUpdate)
@@ -288,20 +315,26 @@ namespace OMVIS
         /// \todo Return statement is missing.
         fmi1_base_type_enu_t getFMI1baseTypeFor4CharString(const std::string& typeString)
         {
-            if (!typeString.compare("real"))
-                return fmi1_base_type_real;
-            else if (!typeString.compare("bool"))
-                return fmi1_base_type_bool;
-            else if (!typeString.compare("inte"))
-                return fmi1_base_type_int;
-            else if (!typeString.compare("stri"))
-                return fmi1_base_type_str;
-            else
+            if (0 == typeString.compare("real"))
             {
-                std::cout << "getFMI1baseTypeFor4CharString failed" << std::endl;
-                return fmi1_base_type_enum;
+                return fmi1_base_type_real;
             }
+            if (0 == typeString.compare("bool"))
+            {
+                return fmi1_base_type_bool;
+            }
+            if (0 == typeString.compare("inte"))
+            {
+                return fmi1_base_type_int;
+            }
+            if (0 == typeString.compare("stri"))
+            {
+                return fmi1_base_type_str;
+            }
+
+            std::cout << "getFMI1baseTypeFor4CharString failed" << std::endl;
+            return fmi1_base_type_enum;
         }
 
-    }  // End namespace Model
-}  // End namespace OMVIS
+    }  // namespace Model
+}  // namespace OMVIS
